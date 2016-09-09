17e31e4df58bd5ffe494f43e5bf7db4ed8affe83
anzhuo <673429405@qq.com>
2016-09-09 17:06:02 +0800

    LT

--- app/src/main/java/com/example/anzhuo/map/Activity/SideslipActivity.java
+++ app/src/main/java/com/example/anzhuo/map/Activity/SideslipActivity.java
@@ -2,6 +2,8 @@ package com.example.anzhuo.map.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +15,17 @@ import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.example.anzhuo.map.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -22,6 +35,35 @@ import java.util.Date;
 public class SideslipActivity extends Activity implements AMapLocationListener{
     public AMapLocationClient mLocationClient=null;
     public AMapLocationClientOption mLocationOption=null;
+    StringBuffer buffer;
+    String url;
+    private static final int MSG = 1;
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MSG:
+                    try {
+
+                        JSONObject jso = new JSONObject(buffer.toString());
+                        JSONObject jsonObject1 = jso.getJSONObject("result");
+                        JSONObject jsonObject2 = jsonObject1.getJSONObject("data");
+                        JSONObject jsonObject3 = jsonObject2.getJSONObject("realtime");
+                        JSONObject jsonObject4=jsonObject3.getJSONObject("weather");
+                        String aaa = jsonObject4.getString("info");
+                        Log.i("LT",aaa);
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +82,19 @@ public class SideslipActivity extends Activity implements AMapLocationListener{
         AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);
         mLocationClient.setLocationOption(mLocationOption);
         mLocationClient.startLocation();
+        new Thread() {
+            @Override
+            public void run() {
+
+                super.run();
+                try {
+                    url = "http://op.juhe.cn/onebox/weather/query?cityname=" + URLEncoder.encode("武汉", "utf-8") + "&dtype=json&key=a36808314ca92f163ec77ef96438e006";
+                    requestNetWork(url);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
 
 
@@ -79,5 +134,25 @@ public class SideslipActivity extends Activity implements AMapLocationListener{
         }
 
 
+    }
+    private void requestNetWork(String url) {
+        try {
+            URL urlstr = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlstr.openConnection();
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            buffer = new StringBuffer();
+            byte[] b = new byte[10 * 1024];
+            int len;
+            Log.i("LT", buffer.toString());
+            while ((len = in.read(b)) != -1) {
+                buffer.append(new String(b, 0, len));
+            }
+            in.close();
+            handler.sendEmptyMessage(MSG);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
