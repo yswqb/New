45ae4a278d0120565d977e76f3dd5a66547e8b9e
anzhuo <673429405@qq.com>
2016-09-12 14:32:09 +0800

    LT1

--- app/src/main/java/com/example/anzhuo/map/Activity/MainActivity.java
+++ app/src/main/java/com/example/anzhuo/map/Activity/MainActivity.java
@@ -1,19 +1,9 @@
-package com.example.anzhuo.map.Activity;
+package com.example.anzhuo.map.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
 import com.example.anzhuo.map.R;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
